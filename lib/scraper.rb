930a28b8dd13caced6fa225abd4533595ec4e667
Nathan Rathe <nprathe10@gmail.com>
2019-07-26 17:14:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,11 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
+doc.css(".site-header__hero__headline")text
 
+doc.css(".tout__label.heading.heading--level-4")
 
+courses.each do |course|
+  puts course.text.strip
+end
